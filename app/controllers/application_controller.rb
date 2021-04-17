ae23fe4e6c4f6a8d96bc79ca2bd09a7e5b3f5052
Stephane Simcic <simcic.stephane@gmail.com>
2021-04-18 05:20:39 +0800

    Creates dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :url])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 end
